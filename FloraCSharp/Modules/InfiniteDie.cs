67e582804fbaa3b506e8d8a49ee922e86e37f97a
CynicalPopcorn <cynicalpopcorn@gmail.com>
2018-01-16 00:08:12 +0000

    Added Infinite-Die support. It's back again.

---
+++ FloraCSharp/Modules/InfiniteDie.cs
@@ -0,0 +1,72 @@
+﻿using Discord;
+using Discord.Commands;
+using FloraCSharp.Extensions;
+using FloraCSharp.Services;
+using FloraCSharp.Services.ExternalDB;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraCSharp.Modules
+{
+    [RequireContext(ContextType.Guild)]
+    [Group("Infinite-Die")]
+    public class InfiniteDie : ModuleBase
+    {
+        private FloraDebugLogger _logger;
+        private readonly FloraRandom _random;
+        private DBconnection _conn;
+
+        public InfiniteDie(FloraRandom random, FloraDebugLogger logger)
+        {
+            _random = random;
+            _logger = logger;
+            _conn = DBconnection.Instance();
+            _conn.DBName = "cynicalp_weebnation";
+        }
+
+        [Command("Get"), Summary("Returns a random side (or optionally a given side")]
+        public async Task Get()
+        {
+            ulong DiceID = 0;
+            string Content = String.Empty;
+            ulong UserID = 0;
+            ulong DiceNumber = 0;
+
+            if (_conn.IsConnected())
+            {
+                string query = "SELECT * FROM content ORDER BY RAND() LIMIT 1";
+                var cmd = new MySqlCommand(query, _conn.Connection);
+                var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    DiceID = (ulong)reader.GetInt64(1);
+                    Content = reader.GetString(2);
+                }
+
+                query = "SELECT * FROM dice WHERE DiceID=@did";
+                cmd = new MySqlCommand(query, _conn.Connection);
+                cmd.Parameters.AddWithValue("@did", DiceID);
+
+                while (await reader.ReadAsync())
+                {
+                    UserID = (ulong)reader.GetInt64(3);
+                    DiceNumber = (ulong)reader.GetInt64(2);
+                }
+                _conn.Close();
+            }
+
+            //Try to resolve user
+            IGuildUser user = await Context.Guild.GetUserAsync(UserID);
+            string username = String.Empty;
+            if (user == null)
+                username = UserID.ToString();
+            else
+                username = user.Username;
+
+            await Context.Channel.SendSuccessAsync("Infinite Die | Side: " + DiceNumber.ToString(), Content, null, "Dice Owner: " + username);
+        }
+    }
+}
---
+++ FloraCSharp/Services/ExternalDB/DBconnection.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+
+namespace FloraCSharp.Services.ExternalDB
+{
+    public class DBconnection
+    {
+        private DBconnection()
+        {
+
+        }
+
+        private string dbName = string.Empty;
+        public string DBName
+        {
+            get { return dbName; }
+            set { dbName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBconnection _instance = null;
+        public static DBconnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBconnection();
+            return _instance;
+        }
+
+        public bool IsConnected()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(dbName))
+                    return false;
+                string[] details = File.ReadAllLines("data/extdb.txt");
+
+                MySqlConnectionStringBuilder connstring = new MySqlConnectionStringBuilder();
+                connstring.Server = details[0];
+                connstring.UserID = details[1];
+                connstring.Password = details[2];
+                connstring.Database = dbName;
+
+                connection = new MySqlConnection(connstring.ToString());
+                connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
