53248fa1f51ec06ccf7aa5ca70024a2ae4054ffb
CynicalPopcorn <cynicalpopcorn@gmail.com>
2018-01-26 12:46:23 +0000

    Massive cleanup

--- FloraCSharp/Modules/Games/Games.cs
+++ FloraCSharp/Modules/Games/Games.cs
@@ -17,7 +17,7 @@ namespace FloraCSharp.Modules.Games
         private readonly FloraRandom _random;
         private FloraDebugLogger _logger;
         private readonly BotGameHandler _botGames;
-        private ConcurrentDictionary<ulong, RNGHandler> ActiveRNG { get; } = new ConcurrentDictionary<ulong, RNGHandler>();
+        private Services.RNGService _rngservice = new Services.RNGService();
 
         public Games(FloraRandom random, FloraDebugLogger logger, BotGameHandler botGames)
         {
@@ -30,12 +30,6 @@ namespace FloraCSharp.Modules.Games
         [RequireContext(ContextType.Guild)]
         public async Task RNGGame([Summary("The minimum, inclusive bound")] int min, [Summary("The maximum, exclusive bound")] int max, int timeout = 30)
         {
-            if (ActiveRNG.ContainsKey(Context.Channel.Id))
-            {
-                await Context.Channel.SendErrorAsync("There is already a game running.");
-                return;
-            }
-
             if (timeout > 0)
             {
                 timeout = timeout * 1000;
@@ -53,72 +47,12 @@ namespace FloraCSharp.Modules.Games
                 Guesses = new HashSet<Guess>()
             };
 
-            if (StartRNGG(Game))
+            if (_rngservice.StartRNGG(Game, (DiscordSocketClient)Context.Client))
             {
                 await Context.Channel.SendSuccessAsync($"RNG Game (Min: {min}, Max: {max})", $"Game started! Type your guesses now! You have {timeout/1000} seconds.");
                 await Task.Delay(timeout);
-                await EndGameInChannel(Context.Guild, Context.Channel);
+                await _rngservice.EndGameInChannel(Context.Guild, Context.Channel, _random);
             }
-        }
-
-        public async Task EndGameInChannel(IGuild guild, IMessageChannel ChannelID)
-        {
-            if (!ActiveRNG.ContainsKey(ChannelID.Id))
-            {
-                await Context.Channel.SendErrorAsync("There is no running game in this channel.");
-                return;
-            }
-
-            RNGGame game = StopRNGG(ChannelID.Id);
-
-            if (game != null)
-            {
-                int roll = _random.Next(game.MinGuess, game.MaxGuess + 1);
-                if (game.Guesses.Select(x => x.GuessIndex == roll).FirstOrDefault())
-                {
-                    var winnerID = game.Guesses.First(x => x.GuessIndex == roll);
-                    IGuildUser user = await guild.GetUserAsync(winnerID.UserID);
-
-                    await ChannelID.BlankEmbedAsync(new EmbedBuilder().WithOkColour()
-                        .AddField(new EmbedFieldBuilder().WithName("🎲 Roll").WithValue(roll))
-                        .AddField(new EmbedFieldBuilder().WithName("🎉 Winner").WithValue(user.Username)));
-                }
-                else
-                {
-                    await ChannelID.BlankEmbedAsync(new EmbedBuilder().WithErrorColour()
-                        .AddField(new EmbedFieldBuilder().WithName("🎲 Roll").WithValue(roll))
-                        .AddField(new EmbedFieldBuilder().WithName("🎉 Winner").WithValue("Nobody. That's sad.")));
-                }
-            }
-        }
-
-        public RNGGame StopRNGG(ulong channelID)
-        {
-            if (ActiveRNG.TryRemove(channelID, out var rngg))
-            {
-                rngg.OnVoted -= Rh_onvote;
-                rngg.End();
-                return rngg.Game;
-            }
-            return null;
-        }
-
-        public bool StartRNGG(RNGGame game)
-        {
-            var rh = new RNGHandler(game, (DiscordSocketClient)Context.Client);
-            if (ActiveRNG.TryAdd(game.Channel, rh))
-            {
-                rh.OnVoted += Rh_onvote;
-                return true;
-            }
-            return false;
-        }
-
-        private async Task Rh_onvote(IUserMessage msg, IGuildUser user)
-        {
-            var toDelete = await msg.Channel.SendSuccessAsync($"{user.Username}: Guess counted");
-            toDelete.DeleteAfter(5);
-            try { await msg.DeleteAsync(); } catch { }
         }
     }
 }
---
+++ FloraCSharp/Modules/Games/Services/RNGService.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.WebSocket;
+using FloraCSharp.Extensions;
+using FloraCSharp.Modules.Games.Common;
+using FloraCSharp.Services;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FloraCSharp.Modules.Games.Services
+{
+    public class RNGService
+    {
+        private ConcurrentDictionary<ulong, RNGHandler> ActiveRNG { get; } = new ConcurrentDictionary<ulong, RNGHandler>();
+
+        public async Task EndGameInChannel(IGuild guild, IMessageChannel ChannelID, FloraRandom _random)
+        {
+            RNGGame game = StopRNGG(ChannelID.Id);
+
+            if (game != null)
+            {
+                int roll = _random.Next(game.MinGuess, game.MaxGuess + 1);
+                if (game.Guesses.Select(x => x.GuessIndex == roll).FirstOrDefault())
+                {
+                    var winnerID = game.Guesses.First(x => x.GuessIndex == roll);
+                    IGuildUser user = await guild.GetUserAsync(winnerID.UserID);
+
+                    await ChannelID.BlankEmbedAsync(new EmbedBuilder().WithOkColour()
+                        .AddField(new EmbedFieldBuilder().WithName("🎲 Roll").WithValue(roll))
+                        .AddField(new EmbedFieldBuilder().WithName("🎉 Winner").WithValue(user.Username)));
+                }
+                else
+                {
+                    await ChannelID.BlankEmbedAsync(new EmbedBuilder().WithErrorColour()
+                        .AddField(new EmbedFieldBuilder().WithName("🎲 Roll").WithValue(roll))
+                        .AddField(new EmbedFieldBuilder().WithName("🎉 Winner").WithValue("Nobody. That's sad.")));
+                }
+            }
+        }
+
+        public RNGGame StopRNGG(ulong channelID)
+        {
+            if (ActiveRNG.TryRemove(channelID, out var rngg))
+            {
+                rngg.OnVoted -= Rh_onvote;
+                rngg.End();
+                return rngg.Game;
+            }
+            return null;
+        }
+
+        public bool StartRNGG(RNGGame game, DiscordSocketClient client)
+        {
+            var rh = new RNGHandler(game, client);
+            if (ActiveRNG.TryAdd(game.Channel, rh))
+            {
+                rh.OnVoted += Rh_onvote;
+                return true;
+            }
+            return false;
+        }
+
+        private async Task Rh_onvote(IUserMessage msg, IGuildUser user)
+        {
+            var toDelete = await msg.Channel.SendSuccessAsync($"{user.Username}: Guess counted");
+            toDelete.DeleteAfter(5);
+            try { await msg.DeleteAsync(); } catch { }
+        }
+    }
+}
