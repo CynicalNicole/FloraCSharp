7738963771eb7a5f3bc14e40bb233ae1f291eb32
BuildTools <unconfigured@null.spigotmc.org>
2018-07-17 02:16:12 +0100

    reaction

---
+++ FloraCSharp/Services/ReactionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraCSharp.Services
+{
+    public class ReactionHandler
+    {
+        private readonly DiscordSocketClient _discord;
+
+        public ReactionHandler(
+            DiscordSocketClient discord)
+        {
+            _discord = discord;
+
+            _discord.ReactionAdded += _discord_ReactionAdded;
+        }
+
+        private async Task _discord_ReactionAdded(Discord.Cacheable<Discord.IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
+        {
+            var msg = await arg1.GetOrDownloadAsync();
+
+            if (!msg.Channel.IsNsfw) return;
+
+            if (msg.Reactions[arg3.Emote].IsMe) return;
+
+            await msg.AddReactionAsync(arg3.Emote);
+        }
+    }
+}
