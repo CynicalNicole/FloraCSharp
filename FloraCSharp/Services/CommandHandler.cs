49c3eed3192b555ebf7b56264ddc67a650f6d0ab
BuildTools <unconfigured@null.spigotmc.org>
2018-07-21 22:19:23 +0100

    removed bot random reactions

--- FloraCSharp/Services/CommandHandler.cs
+++ FloraCSharp/Services/CommandHandler.cs
@@ -82,15 +82,6 @@ namespace FloraCSharp
                 await context.Channel.SendMessageAsync(reaction);
                 return;
             }
-
-            if (context.Guild.Id == 199658366421827584 && context.Channel.Id != 199658366421827584)
-            {
-                int rng = _random.Next(0, 100);
-                if (rng > 95)
-                {
-                    await context.Message.AddReactionAsync(context.Guild.Emotes.RandomItem());
-                }
-            }
         }
 
         private async Task DMHandling(SocketCommandContext context)
