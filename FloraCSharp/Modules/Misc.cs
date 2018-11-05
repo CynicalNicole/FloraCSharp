2b92ca2a79f18e6569f79c54ae5fd34925f69c7d
BuildTools <unconfigured@null.spigotmc.org>
2018-11-05 18:40:15 +0000

    aa

--- FloraCSharp/Modules/Misc.cs
+++ FloraCSharp/Modules/Misc.cs
@@ -741,7 +741,7 @@ namespace FloraCSharp.Modules
         [Command("RoleInfo")]
         public async Task RoleInfo(IRole role)
         {
-            var embed = new EmbedBuilder().WithOkColour().WithTitle("Role Information")
+            var embed = new EmbedBuilder().WithOkColour().WithTitle("📜 Role Information")
                 .AddField(efb => efb.WithName("Role Title").WithValue(role.Name).WithIsInline(true))
                 .AddField(efb => efb.WithName("Role Colour").WithValue(role.Color.ToString()).WithIsInline(true))
                 .AddField(efb => efb.WithName("Hoisted?").WithValue(role.IsHoisted ? "✅" : "❌").WithIsInline(true))
