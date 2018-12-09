336cce21cb55cb90cd4d4ebee0f4809ea14f4151
BuildTools <unconfigured@null.spigotmc.org>
2018-12-09 21:00:21 +0000

    aS

--- FloraCSharp/Services/ExternalDB/DBconnection.cs
+++ FloraCSharp/Services/ExternalDB/DBconnection.cs
@@ -42,6 +42,8 @@ namespace FloraCSharp.Services.ExternalDB
                     return false;
                 string[] details = File.ReadAllLines("data/extdb.txt");
 
+                Console.WriteLine($"{details[0]} | {details[1]} | {details[2]}");
+
                 MySqlConnectionStringBuilder connstring = new MySqlConnectionStringBuilder
                 {
                     Server = details[0],
